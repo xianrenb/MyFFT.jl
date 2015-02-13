092c3f5fe2b70b23303c537c4d696998a5434b09
peter <xianrenb@gmail.com>
2015-02-13 20:58:00 +0800

    updated runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,34 @@ end
 @time v_fft = fft(v)
 
 for i = 1:256
-    @test_approx_eq(real(v_myfft[i]), real(v_fft[i]))
-    @test_approx_eq(imag(v_myfft[i]), imag(v_fft[i]))
+    @test_approx_eq_eps(real(v_myfft[i]), real(v_fft[i]), 1e-12)
+    @test_approx_eq_eps(imag(v_myfft[i]), imag(v_fft[i]), 1e-12)
+end
+
+v = rand(256) + im * rand(256)
+
+@time begin
+    v_myfft = copy(v)
+    myfft(v_myfft)
+end
+
+@time v_fft = fft(v)
+
+for i = 1:256
+    @test_approx_eq_eps(real(v_myfft[i]), real(v_fft[i]), 1e-12)
+    @test_approx_eq_eps(imag(v_myfft[i]), imag(v_fft[i]), 1e-12)
+end
+
+v = rand(1024) + im * rand(1024)
+
+@time begin
+    v_myfft = copy(v)
+    myfft(v_myfft)
+end
+
+@time v_fft = fft(v)
+
+for i = 1:1024
+    @test_approx_eq_eps(real(v_myfft[i]), real(v_fft[i]), 1e-12)
+    @test_approx_eq_eps(imag(v_myfft[i]), imag(v_fft[i]), 1e-12)
 end
