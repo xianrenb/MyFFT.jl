f761aa01f57f71cb04cc5c8904571db95d3e67c4
peter <xianrenb@gmail.com>
2015-02-13 20:22:36 +0800

    updated runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,16 @@
 using MyFFT
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+v = rand(256) + im * rand(256)
+
+@time begin
+    v_myfft = copy(v)
+    myfft(v_myfft)
+end
+
+@time v_fft = fft(v)
+
+for i = 1:256
+    @test_approx_eq(real(v_myfft[i]), real(v_fft[i]))
+    @test_approx_eq(imag(v_myfft[i]), imag(v_fft[i]))
+end
