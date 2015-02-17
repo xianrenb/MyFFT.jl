2c67f0db09c565806e244c98237d8d82be91529e
peter <xianrenb@gmail.com>
2015-02-17 18:30:18 +0800

    added Bluestein's algorithm

--- src/MyFFT.jl
+++ src/MyFFT.jl
@@ -2,6 +2,52 @@ module MyFFT
 
 export myfft, myifft
 
+function _myBluestein(x::AbstractArray{Complex{Float64}, 1}, 
+    twiddleBasis::Complex{Float64})
+    n = size(x)[1]
+    m = 1
+
+    while m < (n << 1) - 1
+        m = m << 1
+    end
+
+    a = zeros(Complex{Float64}, m)
+    b = zeros(Complex{Float64}, m)
+    c = Array(Complex{Float64}, m)
+    c[1] = 1 + 0.0im
+    twiddleBasis2 = twiddleBasis * twiddleBasis
+    twiddle = twiddleBasis
+    twiddle2 = twiddle
+    i = 2
+
+    while i <= m
+        c[i] = twiddle2
+        # t^1, t^3, t^5, t^7, t^9,...
+        twiddle = twiddle * twiddleBasis2
+        # t^1, t^4, t^9, t^16, t^25,...
+        twiddle2 = twiddle2 * twiddle
+        i = i + 1
+    end
+
+    a[1:n] = x[1:n] .* c[1:n]
+    b[1:n] = conj(c[1:n])
+    i = 2
+    j = m
+
+    while i <= n
+        b[j] = b[i]
+        i = i + 1
+        j = j - 1
+    end
+
+    copy((c .* _myconv(a, b))[1:n])
+end
+
+function _myconv(a::AbstractArray{Complex{Float64}, 1}, 
+    b::AbstractArray{Complex{Float64}, 1})
+    myifft(myfft(a) .* myfft(b))
+end
+
 function _myfftTask!(x::AbstractArray{Complex{Float64}, 1}, r::Range, 
     result::AbstractArray{Complex{Float64}, 1}, rResult::Range, n::Integer, 
     twiddleBasis::Complex{Float64})
@@ -47,28 +93,34 @@ end
 function myfft(x::AbstractArray{Complex{Float64}, 1})
     n = size(x)[1]
 
-    if (n == 0) || (n & (n-1) != 0)
+    if n == 0
         throw(ArgumentError)
+    elseif n & (n-1) == 0
+        r = 1:n
+        twiddleBasis = e^(-2.0 * pi * im / n)
+        result = Array(Complex{Float64}, n)
+        _myfftTask!(x, r, result, r, n, twiddleBasis)
+    else
+        twiddleBasis = e^(-1.0 * pi * im / n)
+        result = _myBluestein(x, twiddleBasis)
     end
-
-    r = 1:n
-    twiddleBasis = e^(-2.0 * pi * im / n)
-    result = Array(Complex{Float64}, n)
-    _myfftTask!(x, r, result, r, n, twiddleBasis)
     result
 end
 
 function myifft(x::AbstractArray{Complex{Float64}, 1})
     n = size(x)[1]
 
-    if (n == 0) || (n & (n-1) != 0)
+    if n == 0
         throw(ArgumentError)
+    elseif n & (n-1) == 0
+        r = 1:n
+        twiddleBasis = e^(2.0 * pi * im / n)
+        result = Array(Complex{Float64}, n)
+        _myfftTask!(x, r, result, r, n, twiddleBasis)
+    else
+        twiddleBasis = e^(1.0 * pi * im / n)
+        result = _myBluestein(x, twiddleBasis)
     end
-
-    r = 1:n
-    twiddleBasis = e^(2.0 * pi * im / n)
-    result = Array(Complex{Float64}, n)
-    _myfftTask!(x, r, result, r, n, twiddleBasis)
     result/n
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,11 +9,11 @@ println("fft():")
 @time v_fft = fft(v)
 
 for i = 1:256
-    @test_approx_eq_eps(real(v_myfft[i]), real(v_fft[i]), 1e-13 * 256 * log2(256))
-    @test_approx_eq_eps(imag(v_myfft[i]), imag(v_fft[i]), 1e-13 * 256 * log2(256))
+    @test_approx_eq_eps(real(v_myfft[i]), real(v_fft[i]), 1e-12 * 256 * log2(256))
+    @test_approx_eq_eps(imag(v_myfft[i]), imag(v_fft[i]), 1e-12 * 256 * log2(256))
 end
 
-n = [256, 1024, 262144, 1048576]
+n = [1, 2, 4, 256, 1024, 262144, 1048576, 3, 5, 7, 9, 253, 254, 255, 257, 258, 259]
 
 for i in n
     println("N = $(i):")
@@ -24,21 +24,21 @@ for i in n
     @time v_fft = fft(v)
 
     for j = 1:i
-        @test_approx_eq_eps(real(v_myfft[j]), real(v_fft[j]), 1e-13 * i * log2(i))
-        @test_approx_eq_eps(imag(v_myfft[j]), imag(v_fft[j]), 1e-13 * i * log2(i))
+        @test_approx_eq_eps(real(v_myfft[j]), real(v_fft[j]), 1e-12 * i * log2(i))
+        @test_approx_eq_eps(imag(v_myfft[j]), imag(v_fft[j]), 1e-12 * i * log2(i))
     end
 end
 
-n = [256, 1024]
+n = [1, 2, 256, 1024, 253, 254, 255, 257, 258, 259]
 
 for i in n
     println("N = $(i):")
     v = rand(i) + im * rand(i)
     println("myifft(myfft()):")
-    v2 = myifft(myfft(v))
+    @time v2 = myifft(myfft(v))
 
     for j = 1:i
-        @test_approx_eq_eps(real(v), real(v2), 1e-13 * i * log2(i) ^ 2)
-        @test_approx_eq_eps(imag(v), imag(v2), 1e-13 * i * log2(i) ^ 2)
+        @test_approx_eq_eps(real(v), real(v2), 1e-12 * i * log2(i) ^ 2)
+        @test_approx_eq_eps(imag(v), imag(v2), 1e-12 * i * log2(i) ^ 2)
     end
 end
